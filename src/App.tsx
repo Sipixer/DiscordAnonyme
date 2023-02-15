6bdd5a7d13c6a20426e3b1cf98eef9ed1e211bd7
Sylvain <46543145+Sipixer@users.noreply.github.com>
2023-02-15 21:14:02 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const webhookUrl =
+  "https://discord.com/api/webhooks/1075509858624876644/yMaxDYDde4oXxw2_z1tm93dP8kgg3yb2OAMkA9aCrkHofpnroLDj9JPK8_4EH-bWJ4fC";
+function App() {
+  const [message, setMessage] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setFile(file);
+    }
+  };
+  const onMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file!);
+    formData.append("username", getRandomFunnyName());
+    formData.append("content", message);
+    fetch(webhookUrl, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        toast.success("Message envoyé");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <form onSubmit={onSubmit} className="flex items-center gap-5 flex-col">
+        <h1 className="text-3xl">Message anonyme</h1>
+        <textarea
+          value={message}
+          onChange={onMessageChange}
+          placeholder="Votre message"
+          className="p-2 outline-none rounded-md text-black"
+        />
+
+        <label
+          className="block mb-2 text-sm font-medium text-white"
+          htmlFor="file_input"
+        >
+          Upload a fichier
+        </label>
+        <input
+          className="block w-full text-sm border  rounded-lg cursor-pointe text-gray-400 focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400"
+          id="file_input"
+          type="file"
+          onChange={onFileChange}
+        />
+
+        <button className="py-2 px-4 bg-emerald-500 rounded-md">Envoyé</button>
+      </form>
+    </div>
+  );
+}
+
+const funnyNames = [
+  "Balthazar",
+  "Gargamel",
+  "Jabba",
+  "Ziggy",
+  "Fluffy",
+  "Lilou",
+  "Napoleon",
+  "Waldo",
+  "Zelda",
+  "Taco",
+  "Squidward",
+  "Scooby",
+  "Ziggy",
+  "Waldo",
+  "Gizmo",
+  "Pikachu",
+  "Elmo",
+  "Yoda",
+  "Gandalf",
+  "Bilbo",
+  "Frodo",
+  "Dobby",
+  "Gollum",
+  "Sméagol",
+];
+
+function getRandomFunnyName() {
+  const index = Math.floor(Math.random() * funnyNames.length);
+  return funnyNames[index];
+}
+
+export default App;
